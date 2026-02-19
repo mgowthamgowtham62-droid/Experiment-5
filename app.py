e5b87d77cee8fc465282a4aa63c9b1bceaec6f5d
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-02-19 09:31:30 +0530

    Renaming app.py to file_manager.py for Exp 5

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
-# file_manager.py
-import datetime
+# This code creates 'sample.txt' inside the Jenkins Workspace
+with open("sample.txt", "w") as f:
+    f.write("This is a test file for Experiment 5.\n")
+    f.write("Created via Python script.")
 
-filename = "sample.txt"
-
-# Creating and writing to the file
-with open(filename, "w") as f:
-    f.write(f"File created by Python script on: {datetime.datetime.now()}\n")
-    f.write("This is Experiment 5: File Handling Operations.")
-
-print(f"Successfully created {filename}")
+print("Success: sample.txt has been created.")
